3c20636b6ce58d467e1d7f2b62bde82cdf93ec40
Chris Allan <callan@glencoesoftware.com>
2018-10-19 11:51:12 +0100

    Bump sckit-image to 0.13.1 for PyPI Windows binary wheels

--- setup.py
+++ setup.py
@@ -66,7 +66,7 @@ setuptools.setup(
         "pyzmq==15.3.0",
         "raven==6.1.0",
         "requests==2.18.4",
-        "scikit-image==0.13.0",
+        "scikit-image==0.13.1",
         "scikit-learn==0.19.0",
         "scipy==0.19.1"
     ],
