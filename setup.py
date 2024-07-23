743e0f4b4bcdfc6084fae8b37a8b327405c56e79
Beth Cimini <bethac07@users.noreply.github.com>
2024-07-23 19:00:34 +0100

    Bump centrosome

--- setup.py
+++ setup.py
@@ -62,9 +62,9 @@ setuptools.setup(
         "test": ["pytest>=3.3.2,<4"],
     },
     install_requires=[
-        "boto3==1.14.23",
-        "cellprofiler-core",
-        "centrosome==1.2.2",
+        "boto3>=1.12.28",
+        "cellprofiler-core==4.2.6",
+        "centrosome==1.2.3",
         "docutils==0.15.2",
         "h5py==3.6.0",
         "imageio==2.34.0",
