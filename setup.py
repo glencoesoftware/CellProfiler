e1d62da07decc1fcf06d91fbecde53a2634fe143
David Stirling <dstirling@glencoesoftware.com>
2024-12-02 14:23:24 +0000

    4.2.80002 wheel

--- setup.py
+++ setup.py
@@ -64,7 +64,7 @@ setuptools.setup(
     },
     install_requires=[
         "boto3>=1.12.28",
-        "cellprofiler-core==4.2.8",
+        "cellprofiler-core==4.2.80002",
         "centrosome~=1.2.3",
         "docutils==0.15.2",
         "h5py==3.6.0",
